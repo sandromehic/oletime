4bda307b26b77eac9c06b6aec24c2a1b8cf89031
Sandro Mehic <sandromehic@gmail.com>
2016-12-07 15:53:15 +0100

    working implementation for simple conversion

--- lib/oletime.rb
+++ lib/oletime.rb
@@ -1,5 +1,20 @@
 require "oletime/version"
 
 module Oletime
-  # Your code goes here...
+  class Time
+    attr_accessor :time
+    def initialize(year, month=nil, day=nil, hour=nil, minute=nil, second=nil, utc_offset=nil)
+      @time = ::Time.new(year, month, day, hour, minute, second, utc_offset)
+    end
+
+    def self.from_ole(value)
+      self.new(::Time.now.year).tap do |t|
+        t.time = ::Time.at((value - 25569) * 86400).utc
+      end
+    end
+
+    def to_ole
+      (@time.to_f / 86400) + 25569
+    end
+  end
 end
--- spec/oletime_spec.rb
+++ spec/oletime_spec.rb
@@ -1,11 +1,34 @@
 require "spec_helper"
 
 describe Oletime do
+  before(:example) do
+    @time = Oletime::Time.new(2016,10,25,12,51,34.123,"+00:00")
+    @real_time = Time.new(2016,10,25,12,51,34.123,"+00:00")
+    @unix_time = 1477399894.123
+    @ole_time = 42668.535811609
+    @ole_time_ms = 42668.535811608795
+  end
+
   it "has a version number" do
     expect(Oletime::VERSION).not_to be nil
   end
 
-  it "Instantiates correctly" do
-    expect(true).to eq(true)
+  it "instantites correctly current time" do
+    expect(@time.time).to eq(@real_time)
+  end
+
+  it "creates from OLE time correctly" do
+    t = Oletime::Time.from_ole(@ole_time)
+    expect(t.time.to_f).to be_within(0.001).of(@real_time.to_f)
+  end
+
+  it "creates from OLE time in ms correctly" do
+    t = Oletime::Time.from_ole(@ole_time_ms)
+    expect(t.time.to_f).to be_within(0.000001).of(@real_time.to_f)
+  end
+
+  it "converts time to OLE format" do
+    expect(@time.to_ole).to be_within(0.001).of(@ole_time)
+    expect(@time.to_ole).to be_within(0.000001).of(@ole_time_ms)
   end
 end
